15f5cd7eda640942df32ab7fc3a9171db07400ee
MeidoNoHitsuji <popenkodaniil@mail.ru>
2022-06-08 17:47:23 +0700

    (~) добавил трансфер в квадратные скобки

--- form.go
+++ form.go
@@ -5,6 +5,11 @@
 // Package form implements encoding and decoding of application/x-www-form-urlencoded data.
 package form
 
+import (
+	"net/url"
+	"strings"
+)
+
 const (
 	implicitKey = "_"
 	omittedKey  = "-"
@@ -12,3 +17,27 @@ const (
 	defaultDelimiter = '.'
 	defaultEscape    = '\\'
 )
+
+func TrancferToParentheses(values url.Values) url.Values {
+	newValues := url.Values{}
+
+	for key, value := range values {
+		keys := strings.Split(key, ".")
+
+		var newKeys string
+
+		if len(keys) > 1 {
+			newKeys = keys[0]
+			for _, k := range keys[1:] {
+				newKeys += "[" + k + "]"
+			}
+		} else {
+			newKeys = strings.Join(keys, "")
+		}
+		for _, v := range value {
+			newValues.Add(newKeys, v)
+		}
+	}
+
+	return newValues
+}
